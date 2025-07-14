fb247f0d64956ec60563476ec252252082ca5c1f
Артем Комаров <komarov.ar@office.lan>
2025-07-14 15:58:01 +0200

    feat: улучшает стилизацию компонента AddSpace

--- src/components/AddSpace/AddSpace.vue
+++ src/components/AddSpace/AddSpace.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex flex-wrap gap-1 justify-center items-center">
     <p
-      class="text-sm rounded-xl text-[--color-primary] bg-gray-100 py-2 px-4 text-center dark:bg-gray-800"
+      class="text-sm rounded-xl bg-gray-100 py-2 px-4 text-center dark:bg-gray-800 text-purple-600 dark:text-purple-400"
     >
       This could be your moment. <br />
       <a href="https://t.me/askomarov" class="underline" target="_blank">Place your ad here.</a>
